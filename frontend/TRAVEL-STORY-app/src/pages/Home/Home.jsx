4ea3ea4b5aca039785aaa48ce706e7fe655f658d
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-01-29 22:24:56 +0800

    navbar

--- frontend/TRAVEL-STORY-app/src/pages/Home/Home.jsx
+++ frontend/TRAVEL-STORY-app/src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import LOGO from '../../assets/images/logo.webp';
+import Navbar from "../../components/Input/Navbar";
+
 
 const Home = () => {
     return (
-        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
-            <img src={LOGO} alt="logo" className="h-9" />
-        </div>
+        <Navbar />
     );
 };
 
